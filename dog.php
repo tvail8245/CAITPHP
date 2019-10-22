af49ad393a8832440f01dffcbb68d3634f1178d3
benl <benl@irontowerpress.com>
2019-10-22 13:50:16 -0700

    examples of dog object, and index controller.

--- dog.php
+++ dog.php
@@ -10,10 +10,25 @@ var $weight;
 var $breed;
 var $has_fleas;
 
-public function de_flea(){
-    if ($this->has_fleas){
-        $this->has_fleas = false;
+    public function de_flea()
+    {
+        if ($this->has_fleas) {
+            $this->has_fleas = 0;
+        }
     }
-}
+
+    public function thin_down($how_much)
+    {
+        if ($this->weight) {
+            $this->weight = $this->weight - $how_much;
+        }
+    }
+
+    public function fatten_up($how_much)
+    {
+        if ($this->weight) {
+            $this->weight = $this->weight + $how_much;
+        }
+    }
 
 }
--- index.php
+++ index.php
@@ -10,6 +10,19 @@ $dog->breed = "chocolate Lab";
 $dog->height = 23;
 $dog->length = 60;
 $dog->name = "Spot";
-$dog->weight = 70;
+$dog->weight = 85;
+
+print_r($dog);
+
+if ($dog->weight >= 80){
+    $dog->thin_down(10);
+} elseif ($dog->weight <= 50)
+{
+    $dog->fatten_up(20);
+}
+$dog->de_flea();
+
+
+echo "</br> \n";
 
 print_r($dog);
