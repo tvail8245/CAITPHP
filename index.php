7e2cebc42c42b03ae4111d13220e1d0223da62ff
benl <benl@irontowerpress.com>
2019-10-22 12:33:27 -0700

    foreach example

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 $schoolArray = array('gbs' => '172.16.20.1','rfs' => '172.16.0.1','cas' => '172.16.30.1');
+$array = array(1,2,3,4,5);
 
+foreach ($array as $key => $value){
+    echo $key;
+    echo " Is : ";
+    echo $value;
+    echo " ";
+}
+
+
+echo "<button onclick='alert(\"hello world\")'> click me</button>";
+/*
 foreach ($schoolArray as $bar => $doof){
     echo "The Ip Address for ";
     echo $bar;
@@ -8,4 +19,5 @@ foreach ($schoolArray as $bar => $doof){
     echo $doof;
     echo "</br> \n";
 }
+*/
 
