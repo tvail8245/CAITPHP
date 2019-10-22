e7b76d675b781e19212eacf4bdeef586fc441846
tvail <tvail8245@gmail.com>
2019-10-22 15:34:15 -0700

    created the results page

--- results.php
+++ results.php
@@ -10,4 +10,17 @@ $dog->height = $_POST['height'];
 $dog->length = $_POST['length'];
 $dog->weight = $_POST['weight'];
 $dog->has_fleas = $_POST['has_fleas'];
+
+if ($dog->has_fleas) {
+    $dog->de_flea();
+}
+
+if ($dog->weight<10){
+    $dog->fatten_up(10);
+}
+
+if ($dog->weight>75){
+    $dog->thin_down(10);
+}
+
 print_r($dog);
