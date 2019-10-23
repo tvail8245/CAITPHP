6ebe46cf0afb5b35609410782564ff0f789df971
tvail <tvail8245@gmail.com>
2019-10-23 15:29:41 -0700

    created the results page

---
+++ Report.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "M3rcielago!";
+$dbname = "dog_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "connection success <br> ";
+}
+
+/*
+$sql = "INSERT INTO dog_features
+    ( name, breed, height, length, weight, has_fleas )
+    VALUES
+    ('$dog->name', '$dog->breed', '$dog->color', '$dog->height', '$dog->length', '$dog->weight', '$dog->has_fleas' )";*/
+
+$sql = "select * from dog_features";
+
+if ($result = $conn->query($sql) == TRUE) {
+    while ($row = $result->fetch_object()) {
+                print_r($row);
+            }
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
